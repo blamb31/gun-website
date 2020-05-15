97c1f944ed7218e7bbd06444ba17571bdb0f6833
Blake Lamb <blamb@motivhealth.com>
2020-05-15 15:01:40 -0600

    Added Auth0 into the app

--- gun-website/server/index.js
+++ gun-website/server/index.js
@@ -3,16 +3,48 @@ const app = express();
 var session = require("express-session");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const massive = require("massive");
+const { auth, requiresAuth } = require("express-openid-connect");
 
-// require("dotenv").config();
+require("dotenv").config();
 
 const { SESSION_SECRET, SERVER_PORT } = process.env;
 
 const testCtrl = require("./controllers/test");
 
-app.listen(4000, () =>
-  console.log(`Server is listening at http://localhost:${4000}`)
+// const db = await massive({
+//   host: "127.0.0.1",
+//   port: 5432,
+//   database: "appdb",
+//   user: "appuser",
+//   password: "apppwd",
+// });
+
+app.listen(SERVER_PORT, () =>
+  console.log(`Server is listening at http://localhost:${SERVER_PORT}`)
 );
+
+const config = {
+  required: false,
+  auth0Logout: true,
+  appSession: {
+    secret: SESSION_SECRET,
+  },
+  baseURL: "http://localhost:4000",
+  clientID: "G8Xu6da8qgjGzGhpMle1fVZIek6kOV1z",
+  issuerBaseURL: "https://blake-lamb.auth0.com",
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get("/", (req, res) => {
+  res.send(req.isAuthenticated() ? "Logged in" : "Logged out");
+});
+app.get("/profile", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.openid.user));
+});
 
 app.use(express.static(`${__dirname}/../build`));
 app.use(express.json());
@@ -25,19 +57,9 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-// //CORS Middleware
-// app.use(function (req, res, next) {
-//   //Enabling CORS
-//   res.header("Access-Control-Allow-Origin", "*");
-//   // res.header(“Access-Control-Allow-Origin”, “*”);
-//   // res.header(“Access-Control-Allow-Methods”, “GET,HEAD,OPTIONS,POST,PUT”);
-//   // res.header(“Access-Control-Allow-Headers”, “Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization”);
-//   next();
-// });
-
 app.use(
   session({
-    secret: "asdasdasd",
+    secret: SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {

bbef6907b06dd172b732ce63835d761cf22d2edc
Blake Lamb <blamb@motivhealth.com>
2020-06-29 23:33:00 -0600

    Created the mongoDB and hooked it up

--- gun-website/src/app/navbar/navbar.component.ts
+++ gun-website/src/app/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../shared/services/auth.service';
 import { tap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { TestServiceService } from '../shared/services/test-service.service';
 
 @Component({
   selector: 'app-navbar',
@@ -11,18 +12,32 @@ import { Observable } from 'rxjs';
 export class NavbarComponent implements OnInit {
   public isLoggedIn: boolean;
   public test: boolean;
+  public date: any;
 
-  constructor(private _auth: AuthService) {}
+  constructor(
+    private _auth: AuthService,
+    private _testSvc: TestServiceService
+  ) {}
 
   ngOnInit() {
     this._auth.userProfile$
       .pipe(
         tap((data) => {
-          console.log(data);
           this.isLoggedIn = data;
         })
       )
       .subscribe();
+    this._testSvc
+      .getTest()
+      .pipe(
+        tap((data: any) => {
+          console.log(data.testReturnObj[0].some_date);
+          this.date = data.testReturnObj[0].some_date;
+        })
+      )
+      .subscribe();
+    // .pipe(tap((data) => console.log({ someTest: data })))
+    // .subscribe();
   }
 
   logout() {
